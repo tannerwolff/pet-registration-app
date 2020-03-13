e1828df8b1d6d1d078eada7b904fef2e75f13894
tannerwolff <twolff@dawson.edu>
2020-03-13 09:43:29 -0600

    added a navigation container and added it to the app.js

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+
+  render(){
+    return (
+
+        <div>
+          <h1>If you love you pets, please get them registered.</h1>
+
+        </div>
+
+    );
+  }
+}
